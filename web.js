e78f42763ba5021a7eb8d208b00626c1d45a0b30
FilipeCarvalho <filipemabpcarvalho@gmail.com>
2013-07-12 01:52:38 +0100

    testing

--- web.js
+++ web.js
@@ -2,8 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+/*
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+});*/
+
+app.readFile('index.html', "utf-8", function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
 
 var port = process.env.PORT || 5000;
