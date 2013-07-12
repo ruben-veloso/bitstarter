665fad1b44a0bab284bb7ab7a55cb2a229bd0d29
FilipeCarvalho <filipemabpcarvalho@gmail.com>
2013-07-12 02:11:15 +0100

    testing

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
 fs = require('fs');
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
 
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
 
+/*
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
@@ -17,4 +28,4 @@ fs.readFile('index.html', 'utf-8', function (err, data) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-});
+});*/
